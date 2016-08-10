9f640ec93dc94467372b5d6567e49540ee35af04
Kristian Kim <iondale0106@gmail.com>
2016-08-09 23:53:04 -0400

    Added layout scss files and header and footer.

---
+++ source/_assets/javascripts/application.js
@@ -0,0 +1,1 @@
+(function(){$(document).ready(function(){var n;n=$("#js-mobile-menu").unbind(),$("#js-navigation-menu").removeClass("show"),n.on("click",function(n){n.preventDefault(),$("#js-navigation-menu").slideToggle(function(){$("#js-navigation-menu").is(":hidden")&&$("#js-navigation-menu").removeAttr("style")})})})}).call(this);
